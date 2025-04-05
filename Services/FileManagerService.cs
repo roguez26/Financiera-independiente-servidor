daf9cef6aca0c355325be585cddf9f4bfba5e5ce
francoroguez <118698321+roguez26@users.noreply.github.com>
2025-04-05 16:52:45 -0600

    añadir servicios

---
+++ Services/FileManagerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Independiente.Services
+{
+    public interface IFilePickerService
+    {
+        string PickFile();
+    }
+
+    public class FilePickerService : IFilePickerService
+    {
+        public string PickFile()
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                return openFileDialog.FileName; 
+            }
+            return null; 
+        }
+    }
+
+}
