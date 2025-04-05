2e8a15c40ae0b6af4a9f6054678c87a5cfd4bedd
francoroguez <118698321+roguez26@users.noreply.github.com>
2025-04-05 16:55:47 -0600

    corregir modelo

--- Model/Bank.cs
+++ Model/Bank.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Independiente.Model
 {
-    public class Bank
+    public class Bank : INotifyPropertyChanged
     {
-        public int BankId { get; set; }
-        public string Name { get; set; }
+        private int _bankId { get; set; }
+
+        private string _name;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public int BankId
+        {
+            get => _bankId;
+            set
+            {
+                if (_bankId != value)
+                {
+                    _bankId = value;
+                    OnPropertyChanged(nameof(BankId));
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         public override string ToString()
         {
--- Model/CreditApplication.cs
+++ Model/CreditApplication.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Independiente.Model
 {
-    public class FinancialData
+    public class CreditApplication
     {
+
     }
 }
